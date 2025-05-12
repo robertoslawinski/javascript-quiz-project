bfe974abd12114879a7b37d2c991e4f7fd223419
Maedeh98a <maedeh.ahmadian98@gmail.com>
2025-05-12 17:24:23 +0200

    solved lab

--- src/question.js
+++ src/question.js
@@ -1,7 +1,34 @@
-class Question {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
+// class Question {
+//     // YOUR CODE HERE:
+//     //
+//     // 1. constructor (text, choices, answer, difficulty)
 
-    // 2. shuffleChoices()
+//     // 2. shuffleChoices()
+
+
+class Question {
+    constructor(text, choices, answer, difficulty) {
+        this.text = text;
+        this.choices = choices;
+        this.answer = answer;
+        this.difficulty = difficulty;
+        
+    }
+
+    shuffleChoices() {
+        let currentIndex = this.choices.length;
+        while(currentIndex != 0){
+            let randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+            var temp = this.choices[currentIndex];
+            this.choices[currentIndex]= this.choices[randomIndex];
+            this.choices[randomIndex] = temp;
+        }
+   
+
 }
+
+}
+
+// let question1 = new Question("food",[1, 7, 4, 5], 4, "hard");
+// console.log(question1.shuffleChoices());
--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,67 @@
 class Quiz {
+
     // YOUR CODE HERE:
     //
     // 1. constructor (questions, timeLimit, timeRemaining)
 
+    constructor(questions, timeLimit, timeRemaining)
+     {  
+        this.questions = questions; 
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0; 
+    }
+
+
     // 2. getQuestion()
+
+    getQuestion() {
+        return this.questions[this.currentQuestionIndex];
+
+    }
     
     // 3. moveToNextQuestion()
 
+    moveToNextQuestion() {
+        this.currentQuestionIndex++;
+    }
+
     // 4. shuffleQuestions()
+
+    shuffleQuestions() {
+        let currentIndex = this.questions.length;
+        while(currentIndex != 0){
+            let randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+            var temp = this.questions[currentIndex];
+            this.questions[currentIndex]= this.questions[randomIndex];
+            this.questions[randomIndex] = temp;
+        }
+   
+    }
+
 
     // 5. checkAnswer(answer)
 
-    // 6. hasEnded()
-}
+    checkAnswer(answer){
+        let currentQuestion = this.getQuestion();
+        if (currentQuestion.answer == answer){
+            this.correctAnswers++;
+        }            
+
+        }   
+
+    hasEnded() {
+        if(this.currentQuestionIndex < this.questions.length){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+    
+
+    }
+
+    
